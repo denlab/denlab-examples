50d39e76b32f87213c82f253a62be24f9c660ebc
Denis Labaye <denis.labaye@fircosoft.com>
2017-09-17 13:51:24 +0200

    make: one pre req for multiple targets

--- make/onePreReqForMultipleTargets/util.mk
+++ make/onePreReqForMultipleTargets/util.mk
@@ -20,6 +20,10 @@ define rul2 =
 :	
 endef
 # <template2> -------------------------------------------------------------------------------
+define nl =
+
+
+endef
 
 allTargets += help
 help: ## show this help 
@@ -33,6 +37,25 @@ sys.info: ## show system info: makevariables: .SHELL, SHELL, .SHELLFLAGS, etc.
 	$(info $(shell jq -cCn '{".SHELL": "$(.SHELL)", "SHELL": "$(SHELL)", ".SHELLFLAGS": "$(.SHELLFLAGS)", foo: "bar", jqQuoteFn: $(jqQuoteFn)}'))
 	$(info allTargets=$(allTargets))
 
+define show.make.variables = 
+	VARS_OLD := $(.VARIABLES)
+	CUR-DIR := $(shell pwd)
+	LOG-DIR := $(CUR-DIR)/make-logs
+$(foreach v,                                        \
+  $(filter-out $(VARS_OLD) VARS_OLD,$(.VARIABLES)), \
+  $(info $(v) = $($(v))))
+endef
+
+sys.vars2: sys.vars
+
+.PHONY: sys.cmd
+sys.cmd: ## show make command line invocation
+	$(info MAKE=$(MAKE) MAKEFLAGS=$(MAKEFLAGS) MAKECMDGOALS=$(MAKECMDGOALS))
+
+.PHONY: sys.vars
+sys.vars: ## show make variables
+	$(show.make.variables)
+
 allTargets += loc 
 loc:                                          ## Count LOC.
 	./lib/src.all | wc -l
@@ -40,7 +63,17 @@ loc:                                          ## Count LOC.
 .PHONY: deps.dot
 allTargets += deps.dot
 deps.dot:                                     ## Produce a graphviz (.dot) of deps
-	$(MAKE) -Bnd -f $(firstword $(MAKEFILE_LIST)) all | make2graph > deps.dot
+	$(MAKE) --dry-run --debug=a -f $(firstword $(MAKEFILE_LIST)) $(t) | make2graph > deps.dot
+
+# $(ifndef t \
+# 	$(error $(nl)\
+# $(nl)\
+# ERROR$(nl)\
+#   - variable t must be defined $(nl)\
+#   - example: make $(MAKECMDGOALS) t=all$(nl)\
+# $(nl)\
+# endif
+# )
 
 allTargets += deps.png
 deps.png: deps.dot                            ## Produce a png of deps
@@ -49,6 +82,13 @@ deps.png: deps.dot                            ## Produce a png of deps
 allTargets += deps.view
 deps.view: deps.png                           ## View the depenceny graphically
 	eog deps.png
+
+.PHONY: deps.view.text.ascii
+deps.view.text.ascii: deps.dot ## view deps in ascii
+	graph-easy --from graphviz -as ascii < deps.dot	
+
+deps.view.text.boxart: deps.dot ## view deps in boxart
+	graph-easy --from graphviz -as boxart < deps.dot	
 
 .PHONY: d
 d:
