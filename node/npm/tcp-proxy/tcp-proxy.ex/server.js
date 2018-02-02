58df05597335584c90ac3d1cbfa3ae406d7e06a3
Denis Labaye <denis.labaye@fircosoft.com>
2018-02-02 18:25:28 +0100

    tcp-proxy

--- node/npm/tcp-proxy/tcp-proxy.ex/server.js
+++ node/npm/tcp-proxy/tcp-proxy.ex/server.js
@@ -1,6 +1,25 @@
-const http = require("http");
+const http = require('http');
+require("http-shutdown").extend();
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
-}).listen(55555);
+var port = process.env.PORT;
+
+var server = http.createServer((request, response) => {
+        let body = [];
+        request.on('data', (chunk) => {
+            body.push(chunk);
+        }).on('end', () => {
+            body = Buffer.concat(body).toString();
+            response.end(eval(body));
+        });
+}).listen(port).withShutdown();
+
+function stop() {
+    server.shutdown(function() {
+        console.log('Everything is cleanly shutdown.');
+    });
+};
+
+process.on( "SIGINT", function() {
+    console.log('CLOSING [SIGINT]');
+    stop();
+} );
