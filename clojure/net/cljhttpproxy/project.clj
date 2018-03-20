1bb0c631035beb00bce44d153ecdb18bba4e8368
Denis Labaye <denis.labaye@fircosoft.com>
2018-03-20 10:53:02 +0100

    inprog

--- clojure/net/cljhttpproxy/project.clj
+++ clojure/net/cljhttpproxy/project.clj
@@ -4,7 +4,11 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
-                 [ring/ring-defaults "0.2.1"]]
+                 [ring/ring-defaults "0.2.1"]
+                 [bmaddy/ring-proxy "3.0.0-SNAPSHOT"]
+                 ;; [tailrecursion/ring-proxy "3.0.0-SNAPSHOT"]
+                 [clj-http "3.8.0"]
+                 ]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler cljhttpproxy.handler/app
          :nrepl   {:start? true}
--- clojure/net/cljhttpproxy/src/cljhttpproxy/handler.clj
+++ clojure/net/cljhttpproxy/src/cljhttpproxy/handler.clj
@@ -1,11 +1,41 @@
 (ns cljhttpproxy.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [tailrecursion.ring-proxy :refer [wrap-proxy]]
+))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
   (route/not-found "Not Found"))
 
+(defn log [m]
+  (println m)
+  (spit "/tmp/log" m :append true)
+)
+
+(defn wrap-log [handler]
+  (fn
+    ([request]
+     (log request)
+     (let [response (handler request)]
+       (log response)
+       response
+       ))
+    ([request respond raise]
+     (log request)
+     (let [response (handler request respond raise)]
+       (log response)
+       response
+       ))))
+(log "hi")
 (def app
-  (wrap-defaults app-routes site-defaults))
+  ;; (-> app-routes
+  ;;     (wrap-defaults site-defaults))
+  ;; (wrap-defaults app-routes site-defaults)
+  (-> app-routes
+      wrap-log
+      (wrap-proxy "/" "http://jira.fircosoft.net")
+      wrap-log
+      )
+  )
