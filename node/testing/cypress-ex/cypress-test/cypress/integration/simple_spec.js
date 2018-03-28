57ce937b806eefe7d312aa28d017f9af026f7c7e
Denis Labaye <denis.labaye@fircosoft.com>
2018-03-28 15:34:28 +0200

    inprog

--- node/testing/cypress-ex/cypress-test/cypress/integration/simple_spec.js
+++ node/testing/cypress-ex/cypress-test/cypress/integration/simple_spec.js
@@ -1,29 +1,47 @@
-describe('My First Test', function() {
-    it('Does not do much!', function() {
-        expect(true).to.equal(true);
-    });
-});
+// describe('My First Test', function() {
+//     it('Does not do much!', function() {
+//         expect(true).to.equal(true);
+//     });
+// });
 
-describe('My First Test', function() {
-    it('Does not do much!', function() {
-        expect(true).to.equal(true);
-    });
-});
+// describe('My First Test', function() {
+//     it('Does not do much!', function() {
+//         expect(true).to.equal(true);
+//     });
+// });
 
-describe('My First Test', function() {
+// describe('My First Test', function() {
+//     it('Visits the Kitchen Sink', function() {
+//         cy.visit('https://example.cypress.io')
+//         cy.contains('type').click();
+//         cy.url().should('include', '/commands/actions');
+//         cy.get('.action-email')
+//             .type('fake@email.com')
+//             .should('have.value', 'fake@email.com') ;
+//     });
+// });
+
+
+// describe('Another test', function() {
+//     it('Does not do much!', function() {
+//         expect(true).to.equal(true);
+//     });
+// });
+
+
+describe('google', function() {
     it('Visits the Kitchen Sink', function() {
-        cy.visit('https://example.cypress.io')
-        cy.contains('type').click();
-        cy.url().should('include', '/commands/actions');
-        cy.get('.action-email')
-            .type('fake@email.com')
-            .should('have.value', 'fake@email.com') ;
-    });
-});
+        cy.visit('https://google.com');
+        cy.get('#lst-ib').type('stuff');
+        cy.get('#lst-ib').type('{esc}');
+        cy.get('center > [value="Google Search"]').click();
 
-
-describe('Another test', function() {
-    it('Does not do much!', function() {
-        expect(true).to.equal(true);
+        // cy.contains('cy.get('body')')
+        // cy.pause();
+        // cy.contains('type').click();
+        // cy.url().should('include', '/commands/actions');
+        // cy.get('.action-email')
+        //     .type('fake@email.com')
+        //     .should('have.value', 'fake@email.com') ;
     });
 });
